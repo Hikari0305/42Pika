a893dd4ff41034f1c87188ba8eea283009d773e1
Hikari0305 <78633083+Hikari0305@users.noreply.github.com>
2025-08-18 08:47:58 +0900

    Add files via upload

--- c03/ex04/ft_strstr.c
+++ c03/ex04/ft_strstr.c
@@ -6,37 +6,43 @@
 /*   By: hikari <hikari@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/14 07:52:43 by hikari            #+#    #+#             */
-/*   Updated: 2025/08/14 08:23:46 by hikari           ###   ########.fr       */
+/*   Updated: 2025/08/18 06:21:52 by hikari           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdio.h>
 
-char	*ft_strstr(char *str, char  *to_find)
+char	*ft_strstr(char *str, char *to_find)
 {
 	int i;
 	int j;
-	int k;
 
 	i = 0;
-	j = 0;
-	k = 0;
+	if (to_find[0] == '\0')
+		return (str);
 	while (str[i] != '\0')
 	{
+		j = 0;
+		if (str[i] == to_find[j])
+		{
+			while (str[i + j] == to_find[j] && to_find[j] != '\0')
+			{
+				j++;
+			}
+			if (to_find[j] == '\0')
+				return (&str[i]);
+		}
 		i++;
 	}
-	while (to_find[j] != '\0')
-	{
-		j++;
-	}
-	while (str[k] != '\0' && k < i - j + 1)
-	{
-		
-	}
+	return (0);
 }
+
 
 int main(void)
 {
-    char    a[] = "Hello World";
+    char    a[] = "Hello World Hello";
     char    b[] = "World";
+	char	*result = ft_strstr(a, b);
+	printf("%s", result);
+	return (0);
 }
---
+++ c03/ex05/ft_strlcat.c
@@ -0,0 +1,69 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strlcat.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hikari <hikari@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/08/18 06:36:38 by hikari            #+#    #+#             */
+/*   Updated: 2025/08/18 07:05:27 by hikari           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdio.h>
+
+int	count_dest(char *dest)
+{
+	int i;
+
+	i = 0;
+	while (dest[i] != '\0')
+	{
+		i++;
+	}
+	return (i);
+}
+int	count_src(char *src)
+{
+	int j;
+
+	j = 0;
+	while (src[j] != '\0')
+	{
+		j++;
+	}
+	return (j);
+}
+
+unsigned int	ft_strlcat(char *dest, char *src, unsigned int size)
+{
+	unsigned int	p;
+	int dest_len;
+	int	src_len;
+
+    p = 0;
+	dest_len = count_dest(dest);
+	src_len = count_src(src);
+	if (size == 0)
+		return (src_len);
+	else if (dest_len < size)
+	{
+		while (src[p] != '\0' && dest_len  + p < size - 1)
+		{
+			dest[dest_len + p] = src[p];
+			p++;
+		}
+		dest[dest_len + p] = '\0';
+		return (src_len + dest_len);
+	}
+	else
+		return (src_len + size);
+}
+
+int main(void)
+{
+    char	a[20] = "Hello";
+    char	b[10] = "World";
+    int result = ft_strlcat(a, b, 20);
+	printf("%d", result);
+}
