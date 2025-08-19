237218c736f8b0166b411fd461f37dc7d5222263
Hikari0305 <78633083+Hikari0305@users.noreply.github.com>
2025-08-20 08:53:05 +0900

    Add files via upload

---
+++ c05/ft_fibonacci.c
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_fibonacci.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hikari <hikari@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/08/20 06:55:11 by hikari            #+#    #+#             */
+/*   Updated: 2025/08/20 07:18:27 by hikari           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdio.h>
+
+int	ft_fibonacci(int index)
+{
+	if (index < 0)
+		return (-1);
+	if (index == 0)
+		return (0);
+	else if (index == 1)
+		return (1);
+	return (ft_fibonacci(index - 1) + ft_fibonacci(index - 2));
+}
+
+int main(void)
+{
+	printf("%d", ft_fibonacci(5));
+	return (0);
+}
