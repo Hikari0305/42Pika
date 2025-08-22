f51eceac3fa3afd2e861bc8ceb2c5f65656a2ce2
Hikari0305 <78633083+Hikari0305@users.noreply.github.com>
2025-08-23 08:54:20 +0900

    Add files via upload

--- ft_find_next_prime.c
+++ ft_find_next_prime.c
@@ -5,8 +5,8 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: hikari <hikari@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/08/21 06:17:16 by hikari            #+#    #+#             */
-/*   Updated: 2025/08/21 06:26:30 by hikari           ###   ########.fr       */
+/*   Created: 2025/08/20 08:15:54 by hikari            #+#    #+#             */
+/*   Updated: 2025/08/23 07:35:21 by hikari           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,16 +14,18 @@ int	check_prime(int nb)
 {
 	int	n;
 
-    if (nb % 2 == 0)
-        return (0);
-	n = 3;
+	if (nb % 2 == 0)
+	{
+		return (0);
+	}
+	n = 2;
 	while (n < nb / n)
 	{
 		if (nb % n == 0)
 		{
 			return (0);
 		}
-		n = n + 2;
+		n++;
 	}
 	return (1);
 }
@@ -33,7 +35,7 @@ int	ft_find_next_prime(int nb)
 	if (nb <= 1)
 		return (2);
 	if (nb == 2)
-		return (3);
+		return (2);
 	while (!(check_prime(nb)))
 	{
 		nb++;
@@ -41,9 +43,9 @@ int	ft_find_next_prime(int nb)
 	return (nb);
 }
 
-#include <stdio.h>
-int	main(void)
-{
-	printf("%d", ft_find_next_prime(13));
-	return (0);
-}
+// #include <stdio.h>
+// int	main(void)
+// {
+// 	printf("%d", ft_find_next_prime(13));
+// 	return (0);
+// }
